c0e1c27b9685fcaa03164cad52084de2d8fa621a
asero13th <84772519+asero13th@users.noreply.github.com>
2022-12-29 00:22:34 -0800

    Time: 45 ms (72.58%), Space: 13.8 MB (85.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,12 +4,14 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         i,j,k = m  - 1,n - 1,len(nums1) - 1
-        
-        while i >= 0 and j >= 0:
-            
+        while i >= 0 and j >= 0: 
             nums1[k] = max(nums1[i],nums2[j])
-            if nums1[i] >= nums2[j]: i -= 1
-            else: j -= 1
+            if nums1[i] >= nums2[j]:
+                nums1[k] =nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
             
             k -= 1
             
