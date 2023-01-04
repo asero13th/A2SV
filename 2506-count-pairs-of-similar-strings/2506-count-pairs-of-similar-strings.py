19312ab7e84cf8a69e49e5881e5845109201d06d
asero13th <84772519+asero13th@users.noreply.github.com>
2023-01-04 02:39:21 -0800

    Time: 948 ms (13.44%), Space: 13.9 MB (97.14%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,25 +1,27 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        hashmap = {}
-        ans = 0
-        for word in words:
-            c = list(set(word))
-            c.sort()
-            c = "".join(c)
-            hashmap[c] = hashmap.get(c,0) + 1
-        for word in hashmap:
-            num = hashmap[word]
-            ans += self.permutation(num) if num > 1 else 0
-        return ans
+        count = 0
+        for i in range(len(words)):
+            for j in range(i):
+                if set(words[i]) == set(words[j]):
+                    count +=1
+        return count
+#         hashmap = {}
+#         ans = 0
+        
+#         for word in words:
+#             c = list(set(word))
+#             c.sort()
+#             c = "".join(c)
+#             hashmap[c] = hashmap.get(c,0) + 1
+            
+#         for word in hashmap:
+#             num = hashmap[word]
+#             ans += self.permutation(num) if num > 1 else 0
+#         return ans
             
         
-    def factorial(self,num):
-        if num == 0:
-            return 1
-        else:
-            return num * self.factorial(num - 1)
-    def permutation(self,m):
-        numerator = (m * (m - 1)) // 2
-        
-        return numerator
+#     def permutation(self,m):
+#         return (m * (m - 1)) // 2
+    
         
