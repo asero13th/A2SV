efe6611550483b69709561b9bef2b7955eac7b7e
asero13th <84772519+asero13th@users.noreply.github.com>
2023-01-04 02:40:04 -0800

    Time: 125 ms (65.83%), Space: 14 MB (71.66%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,27 +1,21 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        count = 0
-        for i in range(len(words)):
-            for j in range(i):
-                if set(words[i]) == set(words[j]):
-                    count +=1
-        return count
-#         hashmap = {}
-#         ans = 0
+        hashmap = {}
+        ans = 0
         
-#         for word in words:
-#             c = list(set(word))
-#             c.sort()
-#             c = "".join(c)
-#             hashmap[c] = hashmap.get(c,0) + 1
+        for word in words:
+            c = list(set(word))
+            c.sort()
+            c = "".join(c)
+            hashmap[c] = hashmap.get(c,0) + 1
             
-#         for word in hashmap:
-#             num = hashmap[word]
-#             ans += self.permutation(num) if num > 1 else 0
-#         return ans
+        for word in hashmap:
+            num = hashmap[word]
+            ans += self.permutation(num) if num > 1 else 0
+        return ans
             
         
-#     def permutation(self,m):
-#         return (m * (m - 1)) // 2
+    def permutation(self,m):
+        return (m * (m - 1)) // 2
     
         
