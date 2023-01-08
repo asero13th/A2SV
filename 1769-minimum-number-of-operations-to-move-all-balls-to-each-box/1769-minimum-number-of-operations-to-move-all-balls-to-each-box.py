87f7a3db5f7803c492ece95146762fcf5e3abc48
asero13th <84772519+asero13th@users.noreply.github.com>
2023-01-08 09:11:10 -0800

    Time: 67 ms (95.49%), Space: 14.2 MB (52.25%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,13 +1,23 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        ans = [0] * len(boxes)
-        index = [i for i in range(len(boxes)) if boxes[i] == '1']
-
-        for idx,char in enumerate(boxes):
-            sum_of_ones  = 0
-            for val in index:
-                sum_of_ones = sum_of_ones + abs(idx - val)
-                
-            ans[idx] = sum_of_ones
+        l, ans = len(boxes), []
+        left, right = 0, sum(i for i in range(l) if boxes[i]=='1')
+        one_left, one_right = 0, boxes.count('1')
+        for i in range(l):
+            ans.append(left+right)
+            if boxes[i]=='1':
+                one_left, one_right = one_left+1, one_right-1
+            left, right = left+one_left, right-one_right
         return ans
+    
+#         ans = [0] * len(boxes)
+#         index = [i for i in range(len(boxes)) if boxes[i] == '1']
+
+#         for idx,char in enumerate(boxes):
+#             sum_of_ones  = 0
+#             for val in index:
+#                 sum_of_ones = sum_of_ones + abs(idx - val)
+                
+#             ans[idx] = sum_of_ones
+#         return ans
         
