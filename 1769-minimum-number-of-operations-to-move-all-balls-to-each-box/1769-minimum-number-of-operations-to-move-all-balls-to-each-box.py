faa5be6281a8338a081440d779730583d4a2c153
asero13th <84772519+asero13th@users.noreply.github.com>
2023-01-08 08:32:20 -0800

    Time: 2257 ms (49.30%), Space: 14.2 MB (85.35%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        ans = [0] * len(boxes)
+        index = [i for i in range(len(boxes)) if int(boxes[i]) == 1]
+
+        for idx,char in enumerate(boxes):
+            sum_of_ones  = 0
+            for val in index:
+                sum_of_ones = sum_of_ones + abs(idx - val)
+                
+            ans[idx] = sum_of_ones
+        return ans
+        
