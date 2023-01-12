9492846bde1fd85d5914901b158006d761f80b8b
asero13th <84772519+asero13th@users.noreply.github.com>
2023-01-12 18:16:48 +0300

    Time: 78 ms (99.45%), Space: 15.2 MB (7.57%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -2,7 +2,7 @@ class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         strs = [list(_) for _ in strs]
         ans = 0
-        print(strs)
+        
         zipped = zip(*(strs))
         for _ in zipped:
             if not sorted(_) == list(_):
