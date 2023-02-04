8a311c305fad24800bdb51901f8c73c408b48f89
asero13th <84772519+asero13th@users.noreply.github.com>
2023-02-04 17:11:45 +0300

    Time: 69 ms (84.20%), Space: 14 MB (28.54%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        hashmap_s1 = {}
+        hashmap_s2  = {}
+        
+        for char in "abcdefghijklmnopqrstuvwxyz":
+            hashmap_s1[char] = 0
+            hashmap_s2[char] = 0
+       
+        for i in range(len(s1)):
+            hashmap_s1[s1[i]] += 1
+            hashmap_s2[s2[i]] += 1
+        
+        i,j = 1,len(s1)
+        for val in hashmap_s1:
+                if hashmap_s1[val] != hashmap_s2[val]:
+                    break
+        else:
+            return True
+        while j < len(s2):
+               
+            hashmap_s2[s2[i - 1]] -= 1
+            hashmap_s2[s2[j]] += 1
+            
+            for val in hashmap_s1:
+                if hashmap_s1[val] != hashmap_s2[val]:
+                    break
+            else:
+                return True
+            
+            i += 1
+            j += 1
+            
+            
+        return False
+        
+        
+            
