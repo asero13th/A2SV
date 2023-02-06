643641924548c09310b6e95785067f7899e83486
asero13th <84772519+asero13th@users.noreply.github.com>
2023-02-07 00:36:33 +0300

    Time: 32 ms (99.01%), Space: 16.5 MB (70.65%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,24 +5,42 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        even = ListNode()
-        odd = ListNode()
+
+        if not head:
+            return head
         
-        evenpointer = even
-        oddpointer = odd
-        node = head
-        idx = 0
+
+        even_head = head.next
         
-        while node:
-            if idx % 2 == 0:
-                evenpointer.next = ListNode(node.val,None)
-                evenpointer = evenpointer.next
-            else:
-                oddpointer.next = ListNode(node.val,None)
-                oddpointer = oddpointer.next
-            node = node.next
-            idx += 1
-        evenpointer.next = odd.next
-        return even.next
+
+        odd, even = head, head.next
+        
+        while odd.next and odd.next.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = even_head
+        
+        return head
+#         even = ListNode()
+#         odd = ListNode()
+        
+#         evenpointer = even
+#         oddpointer = odd
+#         node = head
+#         idx = 0
+        
+#         while node:
+#             if idx % 2 == 0:
+#                 evenpointer.next = ListNode(node.val,None)
+#                 evenpointer = evenpointer.next
+#             else:
+#                 oddpointer.next = ListNode(node.val,None)
+#                 oddpointer = oddpointer.next
+#             node = node.next
+#             idx += 1
+#         evenpointer.next = odd.next
+#         return even.next
                 
         
