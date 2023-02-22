5128e60ed96381e3f6f8910abd11e6f1168ef083
asero13th <84772519+asero13th@users.noreply.github.com>
2023-02-22 23:29:27 +0300

    Time: 526 ms (58.17%), Space: 17.1 MB (31.31%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        n = len(weights)
+        lru_cache(None)
+        
+        def isValid(w):
+            count = 0
+            temp = 0
+            for i in weights:
+                temp += i
+                if(temp > w):
+                    count += 1
+                    temp = i
+                if(count > days):
+                    return 0
+            count += 1
+            return count <= days
+        l, r = max(weights), sum(weights)
+        while(l <= r):
+            mid = (l+r)//2
+            if(isValid(mid)):
+                ans = mid
+                r = mid-1
+            else:
+                l = mid+1
+        return ans
