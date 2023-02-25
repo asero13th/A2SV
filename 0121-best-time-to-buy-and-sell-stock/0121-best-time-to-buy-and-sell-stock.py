31c52be04a856d6f424b588f77f0adf764205246
asero13th <84772519+asero13th@users.noreply.github.com>
2023-02-25 11:27:18 -0800

    Time: 895 ms (99.74%), Space: 24.9 MB (82.91%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        least_so_far = inf
+        over_all_profit = 0
+        profit_if_sold_today = 0
+        for price in prices:
+            if price < least_so_far:
+                least_so_far = price
+            profit_if_sold_today = price-least_so_far
+            if(profit_if_sold_today>over_all_profit):
+                over_all_profit = profit_if_sold_today
+        return over_all_profit
+        
