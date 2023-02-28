a4b8103d0e8f52246063ffec0d35671a87420c90
asero13th <84772519+asero13th@users.noreply.github.com>
2023-02-28 10:58:02 -0800

    Time: 37 ms (50.63%), Space: 13.8 MB (93.47%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        num = list(map(int,str(num)))
+
+        while len(num) > 1:
+            num = list(map(int,str(sum(num))))
+ 
+        return num[0]
+        
