547505a4289242445497ca1233943c83291a0ce5
asero13th <84772519+asero13th@users.noreply.github.com>
2023-03-02 12:07:38 -0800

    Time: 44 ms (18.16%), Space: 13.8 MB (54.06%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        print(2**31 - 1)
+        n = abs(x)
+        num = 0
+        power = len(str(n)) - 1
+        
+        while n > 0:
+            val = n % 10
+            num = num + (val * (10**power))
+            n = n // 10
+            power -= 1
+        if num < -2147483647 or num > 2147483647:
+            return 0
+        return num if x >= 0 else -num
+            
+            
+        
+        
