1077be35939edca81c3ebf4f630e22007b0281d4
asero13th <84772519+asero13th@users.noreply.github.com>
2023-03-08 06:33:15 -0800

    Time: 35 ms (60.53%), Space: 14 MB (20.32%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,30 +6,31 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        array = []
-        def pick(root, arr):
-            nonlocal array
-            if not root:
-                return
-            if not root.right and not root.left:
-                array.append(arr)
-            
-            arr.append(str(root.val))
-            
-            pick(root.left, arr.copy())
-            pick(root.right, arr.copy())
-            
-            
-            return
-            
-        pick(root,[])
-        for idx,string in enumerate(array):
-            array[idx] = ("->").join(string)
-        return array
-            
-            
-            
-            
-            
-            
         
+        def collect(root):
+            if not root:
+                return []
+            
+            if not root.right and not root.left:
+                return [[str(root.val)]]
+
+            left = collect(root.left)
+            right = collect(root.right)
+            
+            # print(left, right)
+            
+            if left:
+                for arr in left: arr.insert(0, str(root.val))
+                    
+            if right:
+                for arr in right: arr.insert(0, str(root.val))
+            
+            left.extend(right)
+            return left
+        
+        ans = collect(root)
+        for i, string in enumerate(ans):
+            ans[i] = "->".join(string)
+        return ans
+                
+                
