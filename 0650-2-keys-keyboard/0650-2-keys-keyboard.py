58dbd0dbddcfdca50fec54f5b908c16bb3fada6c
asero13th <84772519+asero13th@users.noreply.github.com>
2023-04-05 08:38:02 -0700

    Time: 35 ms (72.89%), Space: 13.8 MB (95.58%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        def isprime(n):
+            if n == 1:
+                return False
+            for i in range(2, int(n**0.5) +1):
+                if n % i == 0:
+                    return i
+            return n
+        
+        def solve(n):
+            if n == 1: return 0
+            x = isprime(n)
+            
+            if x == n:
+                return x
+            
+            return x + solve(n//x)
+        return solve(n)
+    
+        
+        
