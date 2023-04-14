a0fe2fe4e414d214c1fca18c77b2f4aa97d43c8f
asero13th <84772519+asero13th@users.noreply.github.com>
2023-04-14 21:56:00 +0300

    Time: 110 ms (43.02%), Space: 18.6 MB (64.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c = Counter(nums)
-        print(c.most_common())
-        new_list = [j[0] for i, j in enumerate(c.most_common()) if i < k]
-        return new_list
+  
+        return [j[0] for i, j in enumerate(Counter(nums).most_common()) if i < k]
+
        
         
