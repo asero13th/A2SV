d20ade3c3e1fde9ab7fd95b3f7c3f4bde10512a0
asero13th <84772519+asero13th@users.noreply.github.com>
2023-04-27 18:01:10 +0300

    Time: 80 ms (6.54%), Space: 16.8 MB (10.24%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def bfs(node):
+            queue = deque([node])
+            visited = set({0})
+      
+            while queue:
+                node  = queue.popleft()
+
+                
+                for i in range(len(node)):
+           
+                    if node[i] not in visited:
+                        visited.add(node[i])
+                        queue.append(rooms[node[i]])
+        
+            print(len(rooms), len(visited))
+            return True if len(visited) == len(rooms) else False
+        return bfs(rooms[0])
+                        
+        
+            
