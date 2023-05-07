b82e23b210d82c8517dd3a4a584f5d7d7f222f00
asero13th <84772519+asero13th@users.noreply.github.com>
2023-05-07 15:47:03 +0300

    Time: 62 ms (49.67%), Space: 18.4 MB (35.48%) - LeetHub

---
+++ 0818-race-car/0818-race-car.py
@@ -0,0 +1,25 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        q = collections.deque([(0, 0, 1)]) # moves, position, vel
+        visited = set()
+
+        while q:
+            moves, position, velocity = q.popleft()
+
+            if position == target:
+                return moves
+            
+            if (position, velocity) in visited:
+                continue
+                
+            visited.add((position, velocity))
+
+            q.append((moves + 1, position + velocity, velocity * 2))
+
+            if (position + velocity > target and velocity > 0) or (position + velocity < target and velocity < 0):
+                velocity = -1 if velocity > 0 else 1
+                q.append((moves + 1, position, velocity))
+        
+        
+        return -1 
+        
