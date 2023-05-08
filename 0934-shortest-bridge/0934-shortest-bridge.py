f7b1db866b60c24bcf44f0a1aebebab00fe89de3
asero13th <84772519+asero13th@users.noreply.github.com>
2023-05-08 19:44:52 +0300

    Time: 497 ms (21.31%), Space: 19.9 MB (19.98%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -34,3 +34,63 @@ class Solution:
                 break
         
         return bfs(q) - 1
+#         def getFirstOne(grid):
+#             for i,row in enumerate(grid):
+#                 for j, val in enumerate(row):
+#                     if val == 1:
+#                         return [i,j]
+        
+#         def getSet(i, j):
+#             n,m = len(grid), len(grid[0])
+        
+#             queue = deque([(i,j)])
+#             visited = set()
+#             direction = [(0,1),(1,0),(0,-1),(-1,0)]
+#             while queue:
+                
+#                 node = queue.popleft()
+#                 visited.add(node)
+#                 for x, y  in direction:
+#                     if 0<=x+i<n and 0<=y+j<m and grid[x+i][y+j] == 1:
+#                         queue.append(x+i, y+j)
+#             return visited
+                    
+            
+#         def bfs(grid):
+#             n,m = len(grid), len(grid[0])
+#             queue = deque()
+#             i, j = getFirstOne(grid)
+#             set1 = getSet(i, j)
+            
+            
+#             for i,row in enumerate(grid):
+#                 for j, val in enumerate(row):
+#                     if ((i,j)) not in set1 and grid[i][j] == 1:
+#                         queue.append(i,j)
+            
+#             visited = set()
+#             direction = [(0,1),(1,0),(0,-1),(-1,0)]
+#             while queue:
+                
+#                 level = 0
+#                 for _ in range(len(queue)):
+#                     i, j = queue.popleft()
+                    
+#                     for x, y in direction:
+#                         if 0<=x+i<n and 0<=y+j<m and grid[x+i][y+j] not in set1:
+#                             queue.append(x+i, y+j)
+                            
+                        
+                        
+                        
+                        
+                        
+            
+            
+            
+            
+            
+            
+#         return bfs(grid)
+                        
+                
