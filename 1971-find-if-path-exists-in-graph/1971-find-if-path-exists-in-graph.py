376076626122ab298888039b436114665838b2c1
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-05-17 18:01:18 +0300

    Time: 2192 ms (42.49%), Space: 108.7 MB (52.99%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,27 +1,33 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def getAdjencyList(arr):
-            hashmap = defaultdict(list)
-            
-            for n, m in edges:
-                hashmap[n].append(m)
-                hashmap[m].append(n)
-            return hashmap
+        graph = {i:i for i in range(n)}
+        rank = {i:1 for i in range(n)}
         
-        graph = getAdjencyList(edges)
-        visited = set()
         
-        def dfs(node):
-            if node == destination:
-                return True
-            visited.add(node)
+        def unioNfind(x):
+            if graph[x] == x:
+                return x
             
-            for val in graph[node]:
-                if val not in visited and  dfs(val):
-                    return True
-            return False
-        return dfs(source)
+            val = unioNfind(graph[x])
+            graph[x] = val
+            return val
+        
+        def solve(source, destination):
+            
+            for x, y in edges:
+                edge1 = unioNfind(x)
+                edge2 = unioNfind(y)
                 
-                
+                if edge1 != edge2:
+                    if rank[edge1] > rank[edge2]:
+                        graph[edge2] = graph[edge1]
+                        rank[edge2] += rank[edge1]
+                        
+                    else:
+                        graph[edge1] = graph[edge2]
+                        rank[edge2] += graph[edge1]
+            return unioNfind(graph[source]) == unioNfind(graph[destination])
+        return solve(source, destination)
+     
             
         
