339f6b42e8146df3168047af0bf41eaa1441ef7e
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-05-20 20:16:00 +0300

    Time: 1767 ms (11.14%), Space: 17.1 MB (29.62%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,45 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        parent = [i for i in range(len(stones))]
+        
+        def find(x):
+            if x == parent[x]:
+                return x
+            
+            parent[x] = find(parent[x])
+            return parent[x]
+        
+        
+        def union(x, y):
+            parent_x = find(x)
+            parent_y = find(y)
+            
+            if parent_x == parent_y:
+                return
+            
+            if parent_x < parent_y:
+                parent[parent_y] = parent_x
+            else:
+                parent[parent_x] = parent_y
+                
+   
+        
+        def is_connected(stone1, stone2):
+            if stone1[0] == stone2[0]:
+                return True
+            if stone1[1] == stone2[1]:
+                return True
+            return False
+        
+        for i, stone1 in enumerate(stones):
+            for j, stone2 in enumerate(stones[i+1:], start=i+1):
+                if is_connected(stone1, stone2):
+                    union(i, j)
+                    
+        total_cnt = 0
+        for i in range(len(parent)):
+            if parent[i] != i:
+                total_cnt += 1
+       
+        return total_cnt
+        
