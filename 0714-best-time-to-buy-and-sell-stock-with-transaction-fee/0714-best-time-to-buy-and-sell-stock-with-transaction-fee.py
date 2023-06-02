b51645250b40cdf66f170f0b78f9b9dbfc75453f
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-06-02 18:41:47 +0300

    Time: 1577 ms (7.13%), Space: 243.6 MB (14.32%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        memo = {}
+        ans = []
+        def dp(status, idx):
+            if idx >= len(prices):
+                return 0
+            
+            
+            if (status, idx) in memo:
+                return memo[(status, idx)]
+            
+            sum = 0
+            if status:
+                sum = max(dp(False, idx + 1) - prices[idx], dp(True, idx + 1))
+                
+                
+            else:
+                sum = max(dp(False, idx + 1), dp(True, idx + 1) + prices[idx] - fee)
+                
+            memo[(status, idx)] = sum
+            return memo[(status, idx)]
+        return dp(True, 0)
+                
+            
+            
+            
+        
