0dc3e1c9cd78851a74fe8f38a542659037553209
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-14 22:57:20 +0300

    Time: 46 ms (71.76%), Space: 16.2 MB (93.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,14 +2,15 @@ class Solution:
     def fib(self, n: int) -> int:
         memo = {}
         
-        
         def dp(n):
             if n == 0:
                 return 0
             if n == 1:
                 return 1
+            
             if n not in memo:
                 memo[n] = dp(n - 1) + dp(n - 2)
             
             return memo[n]
         return dp(n)
+        
