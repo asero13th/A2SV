ef1eca990f4871ba7fba42fc8b5a05c535a3c6ec
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-17 22:02:11 +0300

    Time: 1215 ms (40.65%), Space: 55.3 MB (14.02%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,13 +1,47 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        def adjList(edges):
+            graph = defaultdict(list)
+            for i, j in edges:
+                graph[j].append(i)
+            return graph
+        graph = adjList(edges)
+        return [i for i in range(n) if i not in graph]
         
-        def adjecencyList(matrix):
-            occupied = [0] * n
-            hashmap = defaultdict(list)
-            for key, val in matrix:
-                occupied[val] = 1
-            return occupied
-        ans = adjecencyList(edges)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def adjecencyList(matrix):
+#             occupied = [0] * n
+#             hashmap = defaultdict(list)
+#             for key, val in matrix:
+#                 occupied[val] = 1
+#             return occupied
+#         ans = adjecencyList(edges)
       
-        return [i for i,val in enumerate(ans) if val == 0 ]
+#         return [i for i,val in enumerate(ans) if val == 0 ]
         
