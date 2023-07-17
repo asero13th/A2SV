0a253ea9cd095b8f1af5eacba6ea8f0f38e0813d
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-17 21:49:29 +0300

    Time: 942 ms (5.46%), Space: 57.6 MB (5.05%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,18 +1,33 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        def adjecencyList(matrix):
-            hashmap = defaultdict(list)
-            for key, val in matrix:
-                hashmap[key].append(val)
-                hashmap[val].append(key)
-            return hashmap
         
+        def adjecencyList(matrix):
+            graph = defaultdict(list)
+            for i, j in matrix:
+                graph[i].append(j)
+                graph[j].append(i)
+            return graph
         adjList = adjecencyList(edges)
         print(adjList)
         for key in adjList:
             if len(adjList[key]) > 1:
                 return key
         return -1
+                
+                
+#         def adjecencyList(matrix):
+#             hashmap = defaultdict(list)
+#             for key, val in matrix:
+#                 hashmap[key].append(val)
+#                 hashmap[val].append(key)
+#             return hashmap
+        
+#         adjList = adjecencyList(edges)
+#         print(adjList)
+#         for key in adjList:
+#             if len(adjList[key]) > 1:
+#                 return key
+#         return -1
                 
                     
                     
