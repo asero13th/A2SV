e2c6122afc8591255da2f39e3167607163438ae4
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-17 23:59:17 +0300

    Time: 362 ms (60.60%), Space: 17.9 MB (95.62%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,12 +1,13 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        def adjecencyList(matrix):
-            hashmap = defaultdict(list)
-            for key, val in matrix:
-                hashmap[key].append(val)
-                hashmap[val].append(key)
-            return hashmap
-        adjList = adjecencyList(roads)
+        def adjList(edges):
+            graph = defaultdict(list)
+            
+            for i, j in edges:
+                graph[i].append(j)
+                graph[j].append(i)
+            return graph
+        adjList = adjList(roads)
         maximum = 0
         for key in adjList:
             for inner in adjList:
@@ -16,6 +17,56 @@ class Solution:
                     if inner in adjList[key]:
                         maximum -= 1
         return maximum
+                        
+
+            
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def adjecencyList(matrix):
+#             hashmap = defaultdict(list)
+#             for key, val in matrix:
+#                 hashmap[key].append(val)
+#                 hashmap[val].append(key)
+#             return hashmap
+#         adjList = adjecencyList(roads)
+#         maximum = 0
+#         for key in adjList:
+#             for inner in adjList:
+#                 if inner!= key and maximum < len(adjList[key]) + len(adjList[inner]):
+#                     maximum =  len(adjList[key]) + len(adjList[inner])
+                    
+#                     if inner in adjList[key]:
+#                         maximum -= 1
+#         return maximum
                         
             
         
