c5c966bbbac1cc5262ac0aba2bbbc81620e917f2
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-18 20:46:55 +0300

    Time: 85 ms (84.97%), Space: 16.6 MB (23.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,19 +3,18 @@ class Solution:
         
       
         visited = set()
+        initial = image[sr][sc]
+        def inBound(x, y, grid):
+            return 0 <= x < len(grid) and 0 <= y < len(grid[0])
         
         def dfs(x, y):
             if (x, y) in visited:
                 return 
-            if x < 0 or x > len(image) - 1:
-                return 
-            
-            if y < 0 or y > len(image[0])  - 1:
-                return 
+           
             
             visited.add((x , y))
             
-            if image[x][y] == image[sr][sc]:
+            if inBound(x, y, image) and image[x][y] == initial :
                 dfs(x, y +1 )
                 dfs(x + 1, y)
                 dfs(x, y - 1)
