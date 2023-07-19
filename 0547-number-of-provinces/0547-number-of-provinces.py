21a0b3b47362d9b100b9087b7e80b3aaa3fa3f21
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-19 23:34:39 +0300

    Time: 240 ms (20.61%), Space: 17.6 MB (11.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,30 +1,88 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         def adjList(matrix):
-            hashmap = defaultdict(list)
+            graph = defaultdict(list)
             
             for i in range(len(matrix)):
                 for j in range(len(matrix[0])):
                     if matrix[i][j] == 1:
-                        hashmap[j + 1].append(i + 1)
-            return hashmap
-        hashmap = adjList(isConnected)
-        visited = set()
-        
-        
-        def dfs(vertex):
-            
-            visited.add(vertex)
-            for neighbour in hashmap[vertex]:
-                if neighbour not in visited:
-                    dfs(neighbour)
-            return 1
-        
+                        graph[i].append(j)
+            return graph
+        graph = adjList(isConnected)
         ans = 0
-        for val in hashmap:
-            if val not in visited:
-                ans += dfs(val)
+        visited = set()
+        def dfs(index):
+           
+            
+            visited.add(index)
+            for neigh in graph[index]:
+                if neigh not in visited and index != neigh:
+                    dfs(neigh)
+                    
+            return 1    
+                
+           
+        for neigh in graph:
+            if neigh not in visited:
+                ans += dfs(neigh)
         return ans
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def adjList(matrix):
+#             hashmap = defaultdict(list)
+            
+#             for i in range(len(matrix)):
+#                 for j in range(len(matrix[0])):
+#                     if matrix[i][j] == 1:
+#                         hashmap[j + 1].append(i + 1)
+#             return hashmap
+#         hashmap = adjList(isConnected)
+#         visited = set()
+        
+        
+#         def dfs(vertex):
+            
+#             visited.add(vertex)
+#             for neighbour in hashmap[vertex]:
+#                 if neighbour not in visited:
+#                     dfs(neighbour)
+#             return 1
+        
+#         ans = 0
+#         for val in hashmap:
+#             if val not in visited:
+#                 ans += dfs(val)
+#         return ans
                     
                     
                     
