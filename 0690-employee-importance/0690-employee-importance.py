50be2cb56437cca47c56e94ff59b52e7f406e400
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-19 23:12:30 +0300

    Time: 157 ms (66.06%), Space: 18 MB (18.79%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.py
+++ 0690-employee-importance/0690-employee-importance.py
@@ -9,25 +9,68 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        def adj(matrix):
-            graph = defaultdict(list)
-            sub = defaultdict(int)
-            for employee in matrix:
-                graph[employee.id] = employee.subordinates
-                sub[employee.id] = employee.importance
-            return [graph, sub]
         
-        graph, importance = adj(employees)
-        ans = 0
+        def adjList(edges):
+            graph = defaultdict(list)
+            values = defaultdict(list)
+            for arr in edges:
+                graph[arr.id] = arr.subordinates
+                values[arr.id] = arr.importance
+            return [graph, values]
+        graph, imp = adjList(employees)
         visited = set()
-        def dfs(vertex):
-            nonlocal ans
+        def dfs(index):
             
-            visited.add(vertex)
-            ans += importance[vertex]
-            for val in graph[vertex]:
-                if val not in visited:
-                    dfs(val)
+            visited.add(index)
+            for neigh in graph[index]:
+                if neigh not in visited:
+                    dfs(neigh)
         dfs(id)
+        ans = 0
+        for num in visited:
+            ans += imp[num]
+
         return ans
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def adj(matrix):
+#             graph = defaultdict(list)
+#             sub = defaultdict(int)
+#             for employee in matrix:
+#                 graph[employee.id] = employee.subordinates
+#                 sub[employee.id] = employee.importance
+#             return [graph, sub]
+        
+#         graph, importance = adj(employees)
+#         ans = 0
+#         visited = set()
+#         def dfs(vertex):
+#             nonlocal ans
+            
+#             visited.add(vertex)
+#             ans += importance[vertex]
+#             for val in graph[vertex]:
+#                 if val not in visited:
+#                     dfs(val)
+#         dfs(id)
+#         return ans
             
