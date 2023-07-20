d58aff135a1a7b73f3c3b6dbfc7e765e2ccb64d6
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-07-20 22:16:37 +0300

    Time: 45 ms (99.95%), Space: 18.9 MB (73.14%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -7,28 +7,84 @@
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         ans = []
-
-        def bfs(node):
         
+        def bfs(node):
             queue = deque([node])
-           
+            
+            
             while queue:
                 total = 0
-                len_queue = len(queue)
+                length = len(queue)
                 
-                for i in range(len(queue)):
+                for i in range(length):
+                    
                     node = queue.popleft()
-                    total = total + node.val
+                    total += node.val
                     
                     if node.left:
                         queue.append(node.left)
-
+                        
                     if node.right:
                         queue.append(node.right)
+                        
+                ans.append(total/ length)
+            return ans
+        return bfs(root)
+                    
+                    
+                    
+                    
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+
+#         def bfs(node):
+        
+#             queue = deque([node])
+           
+#             while queue:
+#                 total = 0
+#                 len_queue = len(queue)
+                
+#                 for i in range(len(queue)):
+#                     node = queue.popleft()
+#                     total = total + node.val
+                    
+#                     if node.left:
+#                         queue.append(node.left)
+
+#                     if node.right:
+#                         queue.append(node.right)
                 
                     
-                average = total / len_queue 
-                ans.append((average))
+#                 average = total / len_queue 
+#                 ans.append((average))
                
-        bfs(root)
-        return ans        
+#         bfs(root)
+#         return ans        
