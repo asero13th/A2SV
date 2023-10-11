2f358d53ffb36fb272bbac98311d015950a40257
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-10-11 10:10:17 +0300

    Time: 44 ms (18.21%), Space: 16.3 MB (51.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,19 +1,38 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n, h = len(needle), len(haystack)
-        i, j, nxt = 1, 0, [-1]+[0]*n
-        while i < n:                                # calculate next array
-            if j == -1 or needle[i] == needle[j]:   
-                i += 1
-                j += 1
-                nxt[i] = j
-            else:
-                j = nxt[j]
-        i = j = 0
-        while i < h and j < n:
-            if j == -1 or haystack[i] == needle[j]:
-                i += 1
-                j += 1
-            else:
-                j = nxt[j]
-        return i-j if j == n else -1
+        n = len(needle)
+        target = 0
+        curr = 0
+        
+        if len(haystack) < n:
+            return -1 
+        
+        for i in range(n):
+            target += ((ord(needle[i]) - ord('a')) * pow(26, n - i - 1))        
+            curr += ((ord(haystack[i]) - ord('a')) * pow(26, n - i - 1))
+        
+        
+        
+        left = 0
+        right = n 
+        
+        while right < len(haystack):
+            if curr == target: 
+                return left
+            
+            curr -= ((ord(haystack[left]) - ord('a')) * pow(26, n - 1))
+            curr = curr * 26
+            
+            curr += (ord(haystack[right]) - ord('a'))
+             
+            left += 1
+            right += 1
+            
+            
+            
+        if target == curr:
+            return left
+              
+               
+               
+        return -1
