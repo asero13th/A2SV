283b958da86514791ee3995968c5dbf9d0c639e2
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2023-10-11 15:26:59 +0300

    Time: 49 ms (99.12%), Space: 16.2 MB (92.13%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -13,12 +13,15 @@ class Solution:
             hashmap_s1[s1[i]] += 1
             hashmap_s2[s2[i]] += 1
         
-        i,j = 1,len(s1)
+        
         for val in hashmap_s1:
                 if hashmap_s1[val] != hashmap_s2[val]:
                     break
         else:
             return True
+        
+        
+        i,j = 1,len(s1)
         while j < len(s2):
                
             hashmap_s2[s2[i - 1]] -= 1
