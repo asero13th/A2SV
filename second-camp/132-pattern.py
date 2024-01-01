950c4a258632061a677683bc574678cd7121b24c
Aser Hailu <84772519+asero13th@users.noreply.github.com>
2024-01-01 12:06:20 +0300

    Added solution for 132 Pattern

---
+++ second-camp/132-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        minstack = []
+        for num in nums:
+            if not minstack:
+                minstack.append(num)
+            else:
+                minstack.append(min(minstack[-1],num))
+        stack = []
+        print(minstack)
+        for i in range(len(nums) - 1, -1, -1):
+            
+            while stack  and stack[-1] < nums[i]:
+                if minstack[i] < stack[-1]:
+                    return True
+                stack.pop()
+            stack.append(nums[i])
